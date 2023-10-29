3172800cc8be9d5965fcbc0b4b0e9f9b54138c45
vaishaligupta1 <149264451+vaishaligupta1@users.noreply.github.com>
2023-10-29 22:41:41 +0530

    task 02

---
+++ main.js
@@ -0,0 +1,12 @@
+//console.log(document.body.children)
+//console.log(document.body.firstChild);
+//console.log(document.body.firstElementChild)
+const a=document.getElementById("element").firstElementChild;
+console.log(a);
+console.log(a.nextElementSibling.textContent)
+clickbtn.style.color="red";
+element.style.background="red"
+const b =document.getElementsByTagName("li");
+console.log(b)
+const c =document.getElementsByClassName("list-item");
+console.log(c)
