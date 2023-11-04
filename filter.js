ff547b322eb6e7c8cb7ea3c20525e4c8a25f8be4
vaishaligupta1 <149264451+vaishaligupta1@users.noreply.github.com>
2023-11-04 22:14:53 +0530

    Add files via upload

---
+++ filter.js
@@ -0,0 +1,38 @@
+let form= document.getElementById("addForm")
+form.addEventListener("submit", add_item)
+ function add_item(e)
+{
+    e.preventDefault()
+    let ipt1 = document.getElementById("item").value
+    let ipt2 = document.getElementById("description").value
+    let li= document.createElement("li")
+    li.className="list-group-item"
+    let ull=document.getElementById("items")
+    ull.append(li)
+    li.textContent=`${ipt1}  ${ipt2} `
+    let Deletebutton= document.createElement("button")
+    Deletebutton.className="btn btn-danger btn-sm float-right delete"
+    Deletebutton.appendChild(document.createTextNode("X")) 
+    li.appendChild(Deletebutton)
+    let EditButton= document.createElement("button")
+    EditButton.className="btn btn-info btn-sm float-right"
+    EditButton.appendChild(document.createTextNode("Edit"))
+    li.append(EditButton)
+    Deletebutton.onclick=()=>{
+        ull.removeChild(li)
+    }
+    // function deleteItem(){
+    //     ull.removeChild(li)
+    // }
+    
+    EditButton.onclick=()=>{
+        document.getElementById("item").value=ipt1
+        document.getElementById("description").value=ipt2
+        ull.removeChild(li)
+        // document.getElementById("item").value=itp1
+        // document.getElementById("description").value=itp2
+    }
+   
+
+}
+
