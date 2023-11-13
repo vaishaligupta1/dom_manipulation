748a4b31992165c74508dc62edc00f4f55d830c8
vaishaligupta1 <149264451+vaishaligupta1@users.noreply.github.com>
2023-11-13 22:55:48 +0530

    task11

--- main.js
+++ main.js
@@ -4,9 +4,18 @@ function vaishali(event) {
     var email = document.getElementById("email").value;
     var phone = document.getElementById("phone").value;
     console.log(name, email, phone)
-    localStorage.setItem("name", name)
-    localStorage.setItem("email", email)
-    localStorage.setItem("phone", phone)
-    console.log(localStorage.getItem('name'))
-    console.log(localStorage.getItem('email'));
+    // localStorage.setItem("name", name)
+    // localStorage.setItem("email", email)
+    // localStorage.setItem("phone", phone)
+    // console.log(localStorage.getItem('name'))
+    // console.log(localStorage.getItem('email'));
+    let obj= {
+        name,
+        email,
+        phone
+    }
+    localStorage.setItem("name", JSON.stringify(obj))
+  let a= JSON.parse(localStorage.getItem("name"));
+  console.log(a)
+
   }
