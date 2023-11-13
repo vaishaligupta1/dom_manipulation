804c5ad7a04a36d150f7e7dfbed95ff11f403c41
vaishaligupta1 <149264451+vaishaligupta1@users.noreply.github.com>
2023-11-13 22:42:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-console.dir(document.title);
-
+function vaishali(event) {
+    event.preventDefault();
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    console.log(name, email, phone)
+    localStorage.setItem("name", name)
+    localStorage.setItem("email", email)
+    localStorage.setItem("phone", phone)
+    console.log(localStorage.getItem('name'))
+    console.log(localStorage.getItem('email'));
+  }
