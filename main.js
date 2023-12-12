5d83a8ac51cd8669aff715f16d7e08f0c38010cd
vaishaligupta1 <149264451+vaishaligupta1@users.noreply.github.com>
2023-12-12 11:53:33 +0530

    crud post

--- main.js
+++ main.js
@@ -1,43 +1,16 @@
-function addexpense(event){
-    event.preventDefault()
-    let price= document.getElementById('price').value 
-    let description= document.getElementById('description').value
-    let category= document.getElementById("category").value
-    let obj= {
-        price, description,category
-    }
-    addToStorage(obj)
+function vaishali(event){
+
+event.preventDefault()
+    let mail = document.getElementById('email').value                                          
+    let name= document.getElementById('name').value
+    let number= document.getElementById('phone').value
+
+    let obj={mail,name,number}
+
+    addtocrudcrud(obj)
 }
 
-    function addToStorage(obj)
-    {
-          localStorage.setItem("expenseDeatils",JSON.stringify(obj))
-          let data= JSON.parse(localStorage.getItem("expenseDeatils"))
-          addExpenseToScreen(data)
-    }
-    
-    function addExpenseToScreen(data){
-       let li = document.createElement('li')
-       li.textContent= data.price+data.description+data.category
-       let details= document.getElementById('details')
-       details.appendChild(li)
-       let editbtn = document.createElement('button')
-       editbtn.appendChild(document.createTextNode("Edit"))
-       let deletebtn = document.createElement('button')
-       deletebtn.appendChild(document.createTextNode("Delete"))
-       li.appendChild(editbtn)
-       li.appendChild(deletebtn)
-       editbtn.onclick= ()=>{
-        localStorage.removeItem("expenseDetails")
-        details.removeChild(li)
-         document.getElementById('price').value = data.price
-         document.getElementById('description').value = data.description
-         document.getElementById("category").value = data.category
-         
-       }
-       deletebtn.onclick=()=>{
-        localStorage.removeItem("expenseDetails")
-        details.removeChild(li)
-       }
-       
-    }
+async function addtocrudcrud(obj){
+    let res=await axios.post('https://crudcrud.com/api/15b0f1d958db4aafae1cfc47fae456d2/vaishali',obj)
+    console.log(res)
+}
