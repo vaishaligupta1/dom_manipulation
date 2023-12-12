e0c57af7047d4daef6a67e01fb2f6bf124fd744a
vaishaligupta1 <149264451+vaishaligupta1@users.noreply.github.com>
2023-12-12 12:21:45 +0530

    get details from crud crud

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+function refresh(){
+    axios.get('https://crudcrud.com/api/15b0f1d958db4aafae1cfc47fae456d2/vaishali').then((res)=>{
+        for(let i=0;i<res.data.length;i++)
+        {
+            addusertoscreen(res.data[i]);
+        }
+
+    }).catch((err)=>{
+        console.log('error')
+    })
+
+}
+
+refresh()
 function vaishali(event){
 
 event.preventDefault()
@@ -12,5 +26,24 @@ event.preventDefault()
 
 async function addtocrudcrud(obj){
     let res=await axios.post('https://crudcrud.com/api/15b0f1d958db4aafae1cfc47fae456d2/vaishali',obj)
-    console.log(res)
+    addusertoscreen(res.data)
+
+    
+}
+
+function addusertoscreen(obj){
+    let listchild = document.createElement("li")
+    listchild.textContent= `${obj.mail} ${obj.name} ${obj.phone} ${obj._id}`
+    let parent=document.getElementById('item')
+    parent.appendChild(listchild)
+
+    let deletebtn= document.createElement('button')
+    let editbtn= document.createElement('button')
+    deletebtn.appendChild(document.createTextNode('Delete'))
+
+    editbtn.appendChild(document.createTextNode('Edit'))
+    listchild.appendChild(deletebtn)
+    listchild.appendChild(document.createTextNode('  '))
+    listchild.appendChild(editbtn)
+
 }
